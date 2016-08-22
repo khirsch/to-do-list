8ef9fa25bbb89881a58806542d98846290ddcf1c
chris martinez and katrina hirsch <katrina@>
2016-08-22 15:41:03 -0700

    do everything because we forgot to commit along the way

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var Tasks = function(input){
+  this.input = input;
+}
+
+//////////
+$(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var taskInput = $("#taskInput").val();
+    var newTask = new Tasks(taskInput);
+    console.log(newTask);
+    $("#resultsList").append("<li>" + newTask.input + " <span class='glyphicon glyphicon-remove'></span><span class='glyphicon glyphicon-ok'></span>" + "</li>");
+    $(".glyphicon-ok").click(function(){
+      $(this).parent().toggleClass("completed");
+    });
+    $(".glyphicon-remove").click(function(){
+      $(this).parent().remove();
+    });
+  });
+
+});
