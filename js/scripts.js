d0ed821af82269e615f910b639d3bab81aa59967
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-08-22 20:59:20 -0700

    add function to clear form on submit

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,15 @@ $(function(){
     var taskInput = $("#taskInput").val();
     var newTask = new Tasks(taskInput);
 
+    $('input[type="text"], textarea').val('');
     $("#resultsList").append("<li>" + newTask.input + " <span class='glyphicon glyphicon-remove'></span><span class='glyphicon glyphicon-ok'></span>" + "</li>");
-
     $(".glyphicon-ok").click(function(){
       $(this).parent().toggleClass("completed");
     });
-
     $(".glyphicon-remove").click(function(){
       $(this).parent().remove("li")
     });
-
     $("button").toggleClass("rotate");
-    
+
   });
 });
