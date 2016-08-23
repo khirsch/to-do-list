589b405cdee7df15353cd12d811e1a5fb8fd48c5
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-08-22 20:17:54 -0700

    fix styling and animation

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,12 @@ $(function(){
     event.preventDefault();
     var taskInput = $("#taskInput").val();
     var newTask = new Tasks(taskInput);
-    console.log(newTask);
     $("#resultsList").append("<li>" + newTask.input + " <span class='glyphicon glyphicon-remove'></span><span class='glyphicon glyphicon-ok'></span>" + "</li>");
     $(".glyphicon-ok").click(function(){
       $(this).parent().toggleClass("completed");
     });
     $(".glyphicon-remove").click(function(){
-      $(this).parent().addClass("falling");
+      $(this).parent().remove("li")
     });
     $("button").toggleClass("rotate");
   });
