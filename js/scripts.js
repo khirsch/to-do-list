10642afe48905b6724a2d11acf531c9af4ff7df4
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-08-22 20:51:45 -0700

    remove falling animation

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,22 @@ var Tasks = function(input){
 //User Interface Logic
 $(function(){
   $("form").submit(function(event){
-
     event.preventDefault();
+
     var taskInput = $("#taskInput").val();
     var newTask = new Tasks(taskInput);
+
     $("#resultsList").append("<li>" + newTask.input + " <span class='glyphicon glyphicon-remove'></span><span class='glyphicon glyphicon-ok'></span>" + "</li>");
+
     $(".glyphicon-ok").click(function(){
       $(this).parent().toggleClass("completed");
     });
+
     $(".glyphicon-remove").click(function(){
       $(this).parent().remove("li")
     });
-    $("button").toggleClass("rotate");
-  });
 
+    $("button").toggleClass("rotate");
+    
+  });
 });
